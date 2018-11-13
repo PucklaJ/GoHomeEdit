b5d5f5d60a3f13c5dd4c3e26954ba41677c1e774
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-11-13 21:05:46 +0100

    Added editscene.ui and some small changes

--- editscene.go
+++ editscene.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/PucklaMotzer09/gohomeengine/src/frameworks/GTK"
+	framework "github.com/PucklaMotzer09/gohomeengine/src/frameworks/GTK"
 	"github.com/PucklaMotzer09/gohomeengine/src/frameworks/GTK/gtk"
 	"github.com/PucklaMotzer09/gohomeengine/src/gohome"
 	"github.com/PucklaMotzer09/mathgl/mgl32"
@@ -11,33 +9,36 @@ import (
 )
 
 type EditScene struct {
-	cube gohome.Entity3D
+	cube gohome.Entity3D // Test
 }
 
-func (this *EditScene) Init() {
-	gohome.ErrorMgr.Log("Scene", "EditScene", "Initialised!")
-
+func (this *EditScene) InitGUI() {
 	builder := gtk.BuilderNew()
-	if err := builder.AddFromFile("builder.ui"); err != nil {
+	if err := builder.AddFromFile("editscene.ui"); err != nil {
 		gohome.ErrorMgr.MessageError(gohome.ERROR_LEVEL_ERROR, "Builder", "builder.ui", err)
 		return
 	}
 
 	window := builder.GetObject("window").ToWidget().ToWindow()
-	window.ConfigureParameters()
-	window.ConnectSignals()
 	glarea := builder.GetObject("glarea").ToGLArea()
-	glarea.Configure()
-	gtk.SetGLArea(glarea)
-	/*gtk.CreateGLArea()
-	window.ToContainer().GetChildren().Data().ToWidget().ToGrid().Attach(gtk.GetGLArea().ToWidget(), 1, 1, 1, 1)*/
-	gtk.SetWindow(window)
-	gtkf := gohome.Framew.(*framework.GTKFramework)
-	window.ToWidget().ShowAll()
-	gtkf.AfterWindowCreation(&gohome.MainLop)
 
+	gohome.Framew.(*framework.GTKFramework).InitExternalDefault(&window, &glarea)
+	glarea.ToWidget().Show()
+
+	glarea.ToWidget().SignalConnect("size-allocate", func(widget gtk.Widget) {
+		w, h := widget.GetSize()
+		gohome.Render.SetNativeResolution(uint32(w), uint32(h))
+	})
+}
+
+func (this *EditScene) InitGraphics() {
 	gohome.Render.SetBackgroundColor(colornames.Lime)
 	gohome.Init3DShaders()
+	gohome.RenderMgr.UpdateProjectionWithViewport = true
+	gohome.LightMgr.DisableLighting()
+}
+
+func (this *EditScene) InitTest() {
 	gohome.ResourceMgr.LoadTexture("CubeImage", "cube.png")
 
 	mesh := gohome.Box("Cube", [3]float32{1.0, 1.0, 1.0})
@@ -46,13 +47,15 @@ func (this *EditScene) Init() {
 	this.cube.Transform.Position = [3]float32{0.0, 0.0, -3.0}
 
 	gohome.RenderMgr.AddObject(&this.cube)
-	gohome.LightMgr.DisableLighting()
+}
+
+func (this *EditScene) Init() {
+	this.InitGUI()
+	this.InitGraphics()
+	this.InitTest()
 }
 
 func (this *EditScene) Update(delta_time float32) {
-	if gohome.InputMgr.IsPressed(gohome.MouseButtonLeft) {
-		fmt.Println("Pressed")
-	}
 	this.cube.Transform.Rotation = this.cube.Transform.Rotation.Mul(mgl32.QuatRotate(mgl32.DegToRad(30.0)*delta_time, mgl32.Vec3{0.0, 1.0, 0.0})).Mul(mgl32.QuatRotate(mgl32.DegToRad(30.0)*delta_time, mgl32.Vec3{1.0, 0.0, 0.0}))
 }
 
