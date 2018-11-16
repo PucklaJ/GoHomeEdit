0e731196969d133709a43909d098d6057b87e067
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-11-16 15:54:14 +0100

    Removed Test

--- editscene.go
+++ editscene.go
@@ -4,12 +4,10 @@ import (
 	framework "github.com/PucklaMotzer09/gohomeengine/src/frameworks/GTK"
 	"github.com/PucklaMotzer09/gohomeengine/src/frameworks/GTK/gtk"
 	"github.com/PucklaMotzer09/gohomeengine/src/gohome"
-	"github.com/PucklaMotzer09/mathgl/mgl32"
 	"golang.org/x/image/colornames"
 )
 
 type EditScene struct {
-	cube gohome.Entity3D // Test
 }
 
 func (this *EditScene) InitGUI() {
@@ -47,14 +45,6 @@ func (this *EditScene) InitGraphics() {
 }
 
 func (this *EditScene) InitTest() {
-	gohome.ResourceMgr.LoadTexture("CubeImage", "cube.png")
-
-	mesh := gohome.Box("Cube", [3]float32{1.0, 1.0, 1.0})
-	mesh.GetMaterial().SetTextures("CubeImage", "", "")
-	this.cube.InitMesh(mesh)
-	this.cube.Transform.Position = [3]float32{0.0, 0.0, -3.0}
-
-	gohome.RenderMgr.AddObject(&this.cube)
 }
 
 func (this *EditScene) Init() {
@@ -64,7 +54,6 @@ func (this *EditScene) Init() {
 }
 
 func (this *EditScene) Update(delta_time float32) {
-	this.cube.Transform.Rotation = this.cube.Transform.Rotation.Mul(mgl32.QuatRotate(mgl32.DegToRad(30.0)*delta_time, mgl32.Vec3{0.0, 1.0, 0.0})).Mul(mgl32.QuatRotate(mgl32.DegToRad(30.0)*delta_time, mgl32.Vec3{1.0, 0.0, 0.0}))
 	loadLoadableModels()
 
 	if gohome.InputMgr.JustPressed(gohome.MouseButtonLeft) {
